d860a407ad1eb066f3bdff84c2165219b087112d
Andreii Golovan <andreii.golovan@gmail.com>
2018-08-19 13:47:46 +0300

    render success

--- dist/js/common.js
+++ dist/js/common.js
@@ -35,9 +35,6 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-// let react = require('react');
-// import ReactDOM from 'react-dom';
-
 function ServiceBlock(props) {
   return React.createElement(
     "a",
@@ -68,7 +65,7 @@ var App = function (_React$Component) {
     key: "render",
     value: function render() {
       return this.props.blocks.map(function (block) {
-        return React.createElement(ServiceBlock, { title: block.title, src: block.src });
+        return React.createElement(ServiceBlock, { key: block.id, title: block.title, src: block.src });
       });
     }
   }]);
@@ -77,17 +74,89 @@ var App = function (_React$Component) {
 }(React.Component);
 
 var data = [{
+  id: 1,
   title: 'Accountancy',
   src: 'shape-51.svg'
 }, {
+  id: 2,
   title: 'Associations',
   src: 'shape-52.svg'
 }, {
+  id: 3,
   title: 'Buy/Sell Dental Practice',
   src: 'shape-54.svg'
 }, {
+  id: 4,
   title: 'Clinical Waste Collection',
   src: 'shape-56.svg'
+}, {
+  id: 5,
+  title: 'Computer Service',
+  src: 'shape-57.svg'
+}, {
+  id: 6,
+  title: 'CPR/Basic Life Support Training',
+  src: 'shape-58.svg'
+}, {
+  id: 7,
+  title: 'Dental Recruitment',
+  src: 'shape-61.svg'
+}, {
+  id: 8,
+  title: 'Dental Software',
+  src: 'shape-62.svg'
+}, {
+  id: 9,
+  title: 'Electrical Safety And Testing',
+  src: 'shape-63.svg'
+}, {
+  id: 10,
+  title: 'Marketing',
+  src: 'shape-65.svg'
+}, {
+  id: 11,
+  title: 'Others',
+  src: 'shape-71.svg'
+}, {
+  id: 12,
+  title: 'Repairs',
+  src: 'shape-66.svg'
+}, {
+  id: 13,
+  title: 'Solicitors',
+  src: 'shape-68.svg'
+}, {
+  id: 14,
+  title: 'Surgery Design',
+  src: 'shape-75.svg'
+}, {
+  id: 15,
+  title: 'Training & Courses',
+  src: 'shape-69.svg'
+}, {
+  id: 16,
+  title: 'Web Designers',
+  src: 'shape-72.svg'
+}, {
+  id: 17,
+  title: 'Cleaning Agencies',
+  src: 'shape-55.svg'
+}, {
+  id: 18,
+  title: 'Dental Chair Recovery',
+  src: 'shape-60.svg'
+}, {
+  id: 19,
+  title: 'Equipment Engineering',
+  src: 'shape-64.svg'
+}, {
+  id: 20,
+  title: 'Selling Gold',
+  src: 'shape-67.svg'
+}, {
+  id: 21,
+  title: 'Uniform & Workwear',
+  src: 'shape-70.svg'
 }];
 
 ReactDOM.render(React.createElement(App, { blocks: data }), document.getElementById('root'));
